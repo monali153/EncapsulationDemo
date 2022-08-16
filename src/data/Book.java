41f169f8a7cb24c9fd6a7db3719493209ac47ae3
Monali Sharma <mona.chaudhari444@gmail.com>
2022-08-16 12:15:00 +0530

    created getter/ setter method and print the details

--- src/data/Book.java
+++ src/data/Book.java
@@ -1,4 +1,41 @@
 package data;
 
 public class Book {
+
+    private  String title;
+    private int yearOfPublishing;
+    private long isbn ;
+   private String authorName;
+
+    public void setTitle(String title){
+
+        this.title = title;
+    }
+     public String getTitle(){
+        return title;
+     }
+
+    public void setYearOfPublishing(int yearOfPublishing) {
+        this.yearOfPublishing = yearOfPublishing;
+    }
+
+    public int getYearOfPublishing() {
+        return yearOfPublishing;
+    }
+
+    public void setIsbn(long isbn){
+        this.isbn = isbn;
+    }
+
+    public long getIsbn(){
+        return isbn;
+    }
+
+    public void setAuthorName(String authorName){
+        this.authorName = authorName;
+    }
+
+    public String getAuthorName(){
+        return authorName;
+    }
 }
--- src/main/BookImpl.java
+++ src/main/BookImpl.java
@@ -1,5 +1,23 @@
 package main;
 
+import data.Book;
+
 public class BookImpl {
-    
+
+    public static void main(String[] args) {
+
+        Book object = new Book();
+
+        object.setAuthorName("Herbert Schildth");
+        object.setIsbn(1234567890123L);
+        object.setYearOfPublishing(2021);
+        object.setTitle("Complete Reference 12th Edition");
+
+        System.out.println("object.getTitle() = " + object.getTitle());
+        System.out.println("object.getAuthorName() = " + object.getAuthorName());
+        System.out.println("object.getYearOfPublishing() = " + object.getYearOfPublishing());
+        System.out.println("object.getIsbn() = " + object.getIsbn());
+
+    }
+
 }
